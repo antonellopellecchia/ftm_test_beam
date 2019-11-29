5bd9ee6eaec86b3201b01508873c1f192cb69997
Antonello <anto@anto-t430>
2019-11-29 12:20:18 +0100

    On par with example B1

---
+++ build/CMakeFiles/exampleB1.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/anto/Documents/Dottorato/simulations/geant4/examples/B1/exampleB1.cc" "/home/anto/Documents/Dottorato/simulations/geant4/examples/B1/build/CMakeFiles/exampleB1.dir/exampleB1.cc.o"
+  "/home/anto/Documents/Dottorato/simulations/geant4/examples/B1/src/B1ActionInitialization.cc" "/home/anto/Documents/Dottorato/simulations/geant4/examples/B1/build/CMakeFiles/exampleB1.dir/src/B1ActionInitialization.cc.o"
+  "/home/anto/Documents/Dottorato/simulations/geant4/examples/B1/src/B1DetectorConstruction.cc" "/home/anto/Documents/Dottorato/simulations/geant4/examples/B1/build/CMakeFiles/exampleB1.dir/src/B1DetectorConstruction.cc.o"
+  "/home/anto/Documents/Dottorato/simulations/geant4/examples/B1/src/B1EventAction.cc" "/home/anto/Documents/Dottorato/simulations/geant4/examples/B1/build/CMakeFiles/exampleB1.dir/src/B1EventAction.cc.o"
+  "/home/anto/Documents/Dottorato/simulations/geant4/examples/B1/src/B1PrimaryGeneratorAction.cc" "/home/anto/Documents/Dottorato/simulations/geant4/examples/B1/build/CMakeFiles/exampleB1.dir/src/B1PrimaryGeneratorAction.cc.o"
+  "/home/anto/Documents/Dottorato/simulations/geant4/examples/B1/src/B1RunAction.cc" "/home/anto/Documents/Dottorato/simulations/geant4/examples/B1/build/CMakeFiles/exampleB1.dir/src/B1RunAction.cc.o"
+  "/home/anto/Documents/Dottorato/simulations/geant4/examples/B1/src/B1SteppingAction.cc" "/home/anto/Documents/Dottorato/simulations/geant4/examples/B1/build/CMakeFiles/exampleB1.dir/src/B1SteppingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/anto/Downloads/Programmi/geant4.10.05.p01-install/include/Geant4"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtCore"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
